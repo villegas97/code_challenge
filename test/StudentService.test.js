d09dc128634982a5914393bc1e8653467cd6322e
Luis Villegas <lualviva97@gmail.com>
2022-06-16 00:22:57 -0500

    Unit test para endpoint de consulta de estudiantes con creditos mayor a 500

--- test/StudentService.test.js
+++ test/StudentService.test.js
@@ -11,4 +11,18 @@ describe("Pruebas de unidad de StudentService", () => {
     const emails = StudentController.getStudentEmail();
     expect(emails[1]).toBe("Sexton@visualpartnership.xyz");
   });
+
+  test("3) getStudentCredit() test", () => {
+    const studentsByCredit = StudentController.getStudentByCredit();
+    const warren = {
+      id: "6264d5d89f1df827eb84bb23",
+      name: "Warren",
+      email: "Todd@visualpartnership.xyz",
+      credits: 508,
+      enrollments: ["Visual Thinking Intermedio", "Visual Thinking Avanzado"],
+      previousCourses: 1,
+      haveCertification: true,
+    };
+    expect(studentsByCredit[0]).toMatchObject(warren);
+  });
 });
