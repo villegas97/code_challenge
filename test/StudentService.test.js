f36546172ffbdb05c0ab8ec660df296a487789b7
Luis Villegas <lualviva97@gmail.com>
2022-06-16 00:40:50 -0500

    Linter instalado y ejecutado

--- test/StudentService.test.js
+++ test/StudentService.test.js
@@ -1,28 +1,28 @@
 const StudentController = require("../lib/controllers/StudentController");
 const Reader = require("./../lib/utils/Reader");
 describe("Pruebas de unidad de StudentService", () => {
-  test("1) getAllStudents() test", () => {
-    const students = StudentController.getAllStudents();
-    const file = Reader.readJsonFile("visualpartners.json");
-    expect(students).toMatchObject(file);
-  });
+    test("1) getAllStudents() test", () => {
+        const students = StudentController.getAllStudents();
+        const file = Reader.readJsonFile("visualpartners.json");
+        expect(students).toMatchObject(file);
+    });
 
-  test("2) getStudentEmail() test", () => {
-    const emails = StudentController.getStudentEmail();
-    expect(emails[1]).toBe("Sexton@visualpartnership.xyz");
-  });
+    test("2) getStudentEmail() test", () => {
+        const emails = StudentController.getStudentEmail();
+        expect(emails[1]).toBe("Sexton@visualpartnership.xyz");
+    });
 
-  test("3) getStudentCredit() test", () => {
-    const studentsByCredit = StudentController.getStudentByCredit();
-    const warren = {
-      id: "6264d5d89f1df827eb84bb23",
-      name: "Warren",
-      email: "Todd@visualpartnership.xyz",
-      credits: 508,
-      enrollments: ["Visual Thinking Intermedio", "Visual Thinking Avanzado"],
-      previousCourses: 1,
-      haveCertification: true,
-    };
-    expect(studentsByCredit[0]).toMatchObject(warren);
-  });
+    test("3) getStudentCredit() test", () => {
+        const studentsByCredit = StudentController.getStudentByCredit();
+        const warren = {
+            id: "6264d5d89f1df827eb84bb23",
+            name: "Warren",
+            email: "Todd@visualpartnership.xyz",
+            credits: 508,
+            enrollments: ["Visual Thinking Intermedio", "Visual Thinking Avanzado"],
+            previousCourses: 1,
+            haveCertification: true,
+        };
+        expect(studentsByCredit[0]).toMatchObject(warren);
+    });
 });
